5460a1b4784702026d40052eb806245c78454fe1
Youssef Boulhilat <yboulhilat@gmail.com>
2019-07-09 15:01:21 +0200

    Seconde

---
+++ app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+passport.use(new FacebookStrategy({
+  clientID: "450294218866907",
+  clientSecret: "f3950a4a409067705d249c010de9f7d2",
+  callbackURL: 'https://obscure-crag-47700.herokuapp.com/auth/facebook/callback',
+  profileFields: ['id', 'first_name', 'last_name', 'email']
+
+},
+  function (req, accessToken, refreshToken, profile, done) {
+
+    var state = JSON.parse(req.query.state);
+
+    var mergeData = { ...profile._json, redirectUrl: state.redirectUrl };
+
+    return done(null, mergeData);
+
+  }));
+
+app.use(passport.initialize());
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+/* GET facebook callback. */
+/* GET facebook callback. */
+/* GET facebook callback. */
+/* GET facebook callback. */
+/* GET facebook callback. */
+/* GET facebook callback. */
+/* GET facebook callback. */
+/* GET facebook callback. */
+/* GET facebook callback. */
+
+
+/* GET facebook auth. */
+router.get('/auth/facebook',
+  passport.authenticate('facebook', { scope: 'email', state: JSON.stringify(req.query) })
+);
+
+/* GET facebook callback. */
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { session: false }),
+
+
+  function (req, res) {
+    res.redirect(req.user.redirectUrl
+      + "?userId=" + req.user.id
+      + "&firstName=" + req.user.first_name
+      + "&lastName=" + req.user.last_name
+      + "&email=" + req.user.email);
+  }
+);
+module.exports = router;
