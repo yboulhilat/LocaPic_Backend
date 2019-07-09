14251d27bd0aff730d77f9193e033bb8e89e0fca
Youssef Boulhilat <yboulhilat@gmail.com>
2019-07-09 15:58:14 +0200

    Nxxxew

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/auth/facebook/callback',
 
 
   function (req, res) {
-    res.redirect('https://auth.expo.io/@bouls/LocaPicFront'
+    res.redirect(req.user.redirectUrl
       + "?userId=" + req.user.id
       + "&firstName=" + req.user.first_name
       + "&lastName=" + req.user.last_name
