9ee321fefba9988a3480385950d1761224005773
Youssef Boulhilat <yboulhilat@gmail.com>
2019-07-10 16:17:18 +0200

    Userddddffffffffffk

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/auth/facebook/callback',
       + "?userId=" + req.user.id
       + "&firstName=" + req.user.first_name
       + "&lastName=" + req.user.last_name
-      + "&email=" + req.user.email)
+      + "&email=" + req.user.email);
     UserModel.findOne({
       facebookid: req.user.id,
     }, function (err, user) {
@@ -65,4 +65,5 @@ router.post('/logPosition', function (req, res, next) {
       });
   });
 });
+
 module.exports = router;
