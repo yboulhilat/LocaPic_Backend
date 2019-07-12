c98e477400d93444fc4b5c1f01ee0717d90725fe
Youssef Boulhilat <yboulhilat@gmail.com>
2019-07-12 20:05:07 +0200

    crash

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,7 @@ router.get('/auth/facebook/callback',
     });
   });
 
-/* logPosition. */
+/* logPosition.*/
 router.post('/logPosition', function (req, res, next) {
   UserModel.findOne({
     facebookid: req.body.facebookid,
